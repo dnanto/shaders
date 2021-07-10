f29573f619e13ba80171eefe77f302aee8353c5a
dnanto <dnegron2@gmu.edu>
2021-07-09 22:26:49 -0400

    Update circles.glsl
    formatting

--- circles.glsl
+++ circles.glsl
@@ -10,7 +10,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     vec2 p = (1.0 + floor(uv / d)) * d - r;
 
     if (length(uv - p) < r)
-        col = 0.5 + 0.5 * cos(iTime + uv.xyx + vec3(0,2,4));
+        col = 0.5 + 0.5 * cos(iTime + uv.xyx + vec3(0, 2, 4));
 
     fragColor = vec4(col, 1.0);
 }
