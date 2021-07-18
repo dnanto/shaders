2557e9a04feb31542e229cf1084735cee3244291
dnanto <dnegron2@gmu.edu>
2021-07-17 20:40:02 -0400

    Update ckt_demo.glsl

--- ckt_demo.glsl
+++ ckt_demo.glsl
@@ -1,6 +1,6 @@
 #define width 0.025
-#define h 1.0
-#define k 1.0
+#define h 6.0
+#define k 2.0
 
 float plot(vec2 st, float pct)
 {
@@ -62,19 +62,37 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     float R = 0.075;
     float r = cos(radians(30.0)) * R;
 
-    vec3 col = vec3(1);
-    vec2 c = mat2(2. * r, 0., 0., 3.0 * R) * round(p / vec2(2.0 * r, 3.0 * R));
-    if (inreg(p, c, 6.0, R, radians(30.0)))
-        col = vec3(0.5);
-
     vec2 hvec = vec2(2.0 * r, 0.0 * R);
     vec2 kvec = vec2(1.0 * r, 1.5 * R);
 
     vec2 t1 = h * hvec + k * kvec;
     vec2 t2 = rotmat2(radians(60.0)) * t1;
 
+    vec3 rnd = 0.5 + 0.5 * cos(iTime + p.xyx + vec3(0, 2, 4));
+    vec3 col = vec3(rnd);
+
+    vec2 c = mat2(2.0 * r, 0.0, 0.0, 3.0 * R) * round(p / vec2(2.0 * r, 3.0 * R));
+    vec2 d = mat2(2.0 * r, 0.0, 0.0, 3.0 * R) * floor(p / vec2(2.0 * r, 3.0 * R)) + vec2(1.0 * r, 1.5 * R);
+
+    R -= R * 0.05;
+
+    if (inreg(p, c, 6.0, R, radians(30.0)))
+        col = vec3(0.5);
+
+    if (inreg(p, d, 6.0, R, radians(30.0)))
+        col = vec3(1.0);
+
+    if (inreg(p, vec2(0), 6.0, R, radians(30.0)))
+        col = vec3(0.25);
+
+    if (inreg(p, t1, 6.0, R, radians(30.0)))
+        col = vec3(0.25);
+
+    if (inreg(p, t2, 6.0, R, radians(30.0)))
+        col = vec3(0.25);
+
     if (intri(p, vec2(0), t1, t2))
-        col = mix(col, 0.5 + 0.5 * cos(iTime + p.xyx + vec3(0, 2, 4)), 0.5);
+        col = mix(col, rnd, 0.5);
 
     fragColor = vec4(col, 1.0);
 }
