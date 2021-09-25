c8ec86428ce41789f1ae40d03a72282f2334655f
dnanto <dnegron2@gmu.edu>
2021-09-24 22:15:03 -0400

    Update tri_grid.glsl

--- tri_grid.glsl
+++ tri_grid.glsl
@@ -1,8 +1,3 @@
-float cross2(vec2 p, vec2 q)
-{
-    return p.x * q.y - p.y * q.x;
-}
-
 bool intri(vec2 uv, vec2 v1, vec2 v2, vec2 v3)
 {
     // https://mathworld.wolfram.com/TriangleInterior.html
@@ -31,31 +26,23 @@ bool inreg(vec2 uv, vec2 c, float n, float R, float theta)
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
-
     vec2 uv = fragCoord / iResolution.y;
 
     float s = 0.25;
     float R = s * sqrt(3.0) / 3.0;
     float r = s * sqrt(3.0) / 6.0;
-    float a = 0.5 * s * sqrt(3.0);
+    float a = R + r;
 
-    vec2 hvec = vec2(s, 0);
-    vec2 kvec = vec2(s/2., a);
-    mat2 b = mat2(hvec, kvec);
-    vec2 tri = b * round(inverse(b) * uv);
+    uv.x -= s / 2.0;
+    uv.x += mod(floor(uv.y / a), 2.0) * 0.5 * s;
 
-    vec3 col = vec3(1);
-    vec3 rnd = 0.5 + 0.5 * cos(iTime + uv.xyx + vec3(0, 2, 4));
+    float ci = round(uv.x / s);
+    float ri = floor(uv.y / a);
 
-    // not 100% sure why this works, but it's based on the hex grid solution...
-    bool inhex = inreg(uv, tri, 3.0, R, radians(90.0));
-    if (!inhex)
-        if (cross2(vec2(1.5 * s, a), tri - uv) < 0.)
-            tri += (uv.x > tri.x) ? tri : -hvec;
-        else
-            tri += (uv.x > tri.x) ? tri : -kvec;
+    vec2 p = vec2(s * ci, a * ri + r);
+    vec3 col = 0.5 + 0.5 * cos(iTime + uv.xyx + vec3(0, 2, 4));
 
-    if (inhex || inreg(uv, tri, 3.0, R, radians(90.0))) col = vec3(rnd);
+    col = inreg(uv, p, 3.0, R, radians(90.0)) ? col : vec3(0);
 
     fragColor = vec4(col, 1.0);
 }
