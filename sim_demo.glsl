59154a741ee670fa3410bf091dbafef0d5b8e862
dnanto <dnegron2@gmu.edu>
2021-10-21 11:03:01 -0400

    Update sim_demo.glsl

--- sim_demo.glsl
+++ sim_demo.glsl
@@ -1,6 +1,8 @@
 // Common
 
-#define N 100
+#define N         25
+#define G         0.00025
+#define SOFTENING 0.01
 
 // Buffer A
 
@@ -10,19 +12,47 @@ float random(vec2 st)
     return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
 }
 
+vec2 force_gravity(int i, vec2 pos, float softening) {
+    vec2 f = vec2(0);
+    for (int j = 0; j < N; j++) {
+        if (i != j) {
+            vec4 obj = texelFetch(iChannel0, ivec2(j, 0), 0);
+            vec2 r = obj.xy - pos;
+            f += (1.0 * r) / pow(pow(length(r), 2.0) + softening * softening, 1.5);
+        }
+    }
+    return G * 1.0 * f;
+}
+
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
-    vec2 uv = fragCoord / iResolution.xy;
+    vec2 uv = fragCoord;
+    ivec2 iv = ivec2(uv);
 
     vec4 frag;
     if (iFrame < 60) {
-        uv += vec2(random(vec2(iTime)), random(vec2(iTime*iTime)));
+        uv += vec2(random(vec2(iTime)), random(vec2(iTime * iTime)));
         frag = vec4(
             random(uv.xx), random(uv.xy), // position
             random(uv.yx), random(uv.yy)  // velocity
         );
+        frag.zw *= 0.0025;
     } else {
-        frag = texture(iChannel0, uv);
+        vec4 obj = vec4(0);
+        if (iv.x < N && iv.y == 0) {
+            obj = texelFetch(iChannel0, ivec2(uv), 0);
+            /*
+                f = force(pos)
+                fpos = pos + vel * dt + 0.5 * f * dt ** 2
+                fvel = vel + 0.5 * (f + force(fpos)) * dt
+             */
+            vec2 f1 = force_gravity(iv.x, obj.xy, SOFTENING);
+            obj.xy = obj.xy + obj.zw * iTimeDelta + 0.5 * f1 * iTimeDelta * iTimeDelta;
+            vec2 f2 = force_gravity(iv.x, obj.xy, SOFTENING);
+            obj.zw = obj.zw + 0.5 * (f1 + f2) * iTimeDelta;
+
+        }
+        frag = obj;
     }
 
     fragColor = frag;
@@ -33,13 +63,15 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     // output Buffer A
-    vec2 uv = fragCoord / iResolution.xy;
+    vec2 uv = fragCoord/iResolution.xy;
+
     vec3 col = vec3(0);
     for (int i = 0; i < N; i++) {
-        vec4 data = texture(iChannel0, vec2(float(i) / iResolution.x, 0));
-        if (distance(uv, data.xy) < 0.010)
-            col = vec3(data.xyz);
+        vec4 data = texelFetch(iChannel0, ivec2(i, 0), 0);
+        if (distance(uv, data.xy) < 0.010) {
+            col = data.xyy;
+        }
     }
 
-    fragColor =  vec4(col, 1);
+    fragColor = vec4(col, 1);
 }
